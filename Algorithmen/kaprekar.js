bbf4ac6a478759d7be5376d8d59671d39e72f7b4
TrueKuehli <rctcoaster2000@hotmail.de>
2018-10-27 11:41:37 +0200

    Calculate all possible karprekars now! Also made a check to never allow num to be 0, since this causes an infinite loop.

--- Algorithmen/kaprekar.js
+++ Algorithmen/kaprekar.js
@@ -1,17 +1,27 @@
 function kaprekar(n) {
-  if (n > 9999) return false;
-  while (n != 6174) {
-    let digits = n.toString().padStart(4, '0').split('');
+  let num = n;
+  if (num > 9999) return false;
+  while (num != 6174) {
+    let digits = num.toString().padStart(4, '0').split('');
     let sorted = digits.sort();
     let nUp = parseInt(sorted.join(''));
     let nDown = parseInt(sorted.reverse().join(''));
 
-    n = nDown - nUp;
+    num = nDown - nUp;
 
-    console.log(nDown, nUp);
+    if (num == 0) {
+      // Sollte 0 erreicht werden, soll der Algorithmus beendet werden, da
+      //   dieser sonst nicht terminiert
+      console.log('Breaks for:', n);
+      return false;
+    }
+
+    // console.log(nDown, nUp, num);
   }
 
   return true;
 }
 
-kaprekar(90);
+for (let i = 0; i < 10000; i++) {
+  kaprekar(i);
+}
