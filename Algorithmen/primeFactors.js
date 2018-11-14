84ffb9d0fec64d861545df24ec9e76d1ed64f0a6
Timon Scholz <rctcoaster2000@hotmail.de>
2018-11-14 11:44:44 +0100

    Stuff

--- Algorithmen/primeFactors.js
+++ Algorithmen/primeFactors.js
@@ -1,8 +1,30 @@
-function prime(n) {
-  for (let i = 2; i < Math.sqrt(n); i++) {
-    if (n % i == 0) return prime(i).concat(prime(n / i));
+function prime(n, i=2) {
+  for (i; i < Math.sqrt(n); i++) {
+    if (n % i == 0) return prime(i).concat(prime(n / i, i));
   }
   return [n];
 }
 
-console.log(prime(4680));
+function prime2(n) {
+  let a = 2;
+  let primes = [];
+  while (n > 1) {
+    if (n % a) {
+      primes.push(a);
+      n = n / a;
+    } else a++;
+  }
+  return primes;
+}
+
+console.time('1');
+for (let i = 1; i < 1000000; i++) {
+  prime(i);
+}
+console.timeEnd('1');
+
+console.time('2');
+for (let i = 1; i < 1000000; i++) {
+  prime2(i);
+}
+console.timeEnd('2');
